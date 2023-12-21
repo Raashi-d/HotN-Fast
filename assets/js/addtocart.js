4f1502f113fcbb1694bf241f8b555136e65875fa
Mohamed Raashid <149940076+Raashi-d@users.noreply.github.com>
2023-12-21 11:47:29 +0530

    Add files via upload

---
+++ assets/js/addtocart.js
@@ -0,0 +1,152 @@
+const btnCart=document.querySelector('#cart-icon');
+const cart=document.querySelector('.cart');
+const btnClose=document.querySelector('#cart-close');
+
+
+//open cart
+btnCart.addEventListener('click',()=>{
+    cart.classList.add('cart-active');
+});
+
+//close cart
+btnClose.addEventListener('click',()=>{
+    cart.classList.remove('cart-active');
+});
+
+document.addEventListener('DOMContentLoaded',loadFood);
+
+function loadFood(){
+    loadContent();
+}
+
+function loadContent(){
+    //remove items from cart
+    let btnRemove=document.querySelectorAll('.cart-remove');
+    btnRemove.forEach((btn)=>{
+        btn.addEventListener('click',removeItem);
+    });
+
+    //Prodect count change
+    let qtyElements=document.querySelectorAll('.cart-quantity');
+    qtyElements.forEach((input)=>{
+        input.addEventListener('change',changeQty);
+    });
+
+    //prodect Cart
+    let cartBtn=document.querySelectorAll('.cart-btn');
+    cartBtn.forEach((btn)=>{
+        btn.addEventListener('click',addCart);
+    });
+
+    updateTotal();
+
+}
+
+//Remove items from cart
+function removeItem(){
+    if(confirm('Are you sure you want to remove this item?')){
+        let title=this.parentElement.querySelector('.cart-food-title').innerHTML;
+        itemList=itemList.filter(el=>el.title!=title);
+        this.parentElement.remove();
+        loadContent();
+    }
+}
+
+//Change quantity
+function changeQty(){
+    if(isNaN(this.value) || this.value<1){
+        this.value=1;
+    }
+    loadContent();
+}
+
+let itemList=[];
+
+//Add Cart
+function addCart(){
+    let food=this.parentElement;
+    let title=food.querySelector('.dish-name').innerHTML;
+    let price=food.querySelector('.dish-price').innerHTML;
+    let imgSrc=food.querySelector('.food-img').src;
+    //console.log(title, price, imgSrc);
+
+    let newProduct={title,price,imgSrc}
+
+    //check if product already exists
+    if(itemList.find((el)=>el.title==newProduct.title)){
+        alert("Product already exists");
+        return;
+    }else{
+        itemList.push(newProduct);
+    }
+
+
+    let newProdectElement = createCartProdect(title, price, imgSrc);
+    let element=document.createElement('div');
+    element.innerHTML=newProdectElement
+    let cartBasket=document.querySelector('.cart-content');
+    cartBasket.append(element);
+    loadContent();
+
+}
+
+function createCartProdect(title, price, imgSrc){
+    return `
+    <div class="cart-box">
+          <img src="${imgSrc}" class="cart-img">
+          <div class="ditail-box">
+            <div class="cart-food-title">${title}</div>
+            <div class="price-box">
+              <div class="cart-price">${price}</div>
+              <div class="cart-amt">${price}</div>
+            </div>
+            <input type="number" value="1" class="cart-quantity">
+          </div>
+          <img src="./assets/images/delete.png" name="trash" class="cart-remove">         
+      </div>
+    `;
+}
+
+function updateTotal(){
+    const cartItems=document.querySelectorAll('.cart-box');
+    const totalValue=document.querySelector('.total-price');
+
+    let total=0;
+
+    cartItems.forEach(prodect=>{
+        let priceElement=prodect.querySelector('.cart-price');
+        let price=parseFloat( priceElement.innerHTML.replace("Rs. ",""));
+        let qty=prodect.querySelector('.cart-quantity').value;
+        total+=(price*qty);
+        prodect.querySelector('.cart-amt').innerText="Rs. "+(price*qty);
+    });
+
+    totalValue.innerHTML='Rs. '+total;
+
+
+    //add Product count 
+    const cartCount=document.querySelector('.cart-item-count');
+    let count=itemList.length;
+    cartCount.innerHTML=count;
+
+    if(count==0) {
+        cartCount.style.display='none';
+    }else{
+        cartCount.style.display='block';
+    }
+}
+
+//Bill payment
+const btnPayment=document.querySelector('.buy-btn');
+const billing=document.querySelector('.billing');
+const btnClosePayment=document.querySelector('.close-icon');
+
+//open billing
+btnPayment.addEventListener('click',()=>{
+    billing.classList.add('billing-active');
+});
+
+//close billing
+btnClosePayment.addEventListener('click',()=>{
+    billing.classList.remove('billing-active');
+});
---
+++ hotnfast.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 19, 2023 at 05:09 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotnfast`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fname` varchar(50) NOT NULL,
+  `Uname` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Pnumber` int NOT NULL,
+  `pword` varchar(50) NOT NULL,
+  `Confirmpw` varchar(50) NOT NULL,
+  `gender` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`id`, `fname`, `Uname`, `Email`, `Pnumber`, `pword`, `Confirmpw`, `gender`) VALUES
+(4, 'Mohamed Raashid', 'Raashid', 'mhmdrashid@gmail.com', 771234567, 'Raashid', 'Raashid', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
