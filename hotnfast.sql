afa7db9e9c13d9d2d9e297db2bb1f6736550b76f
Raashi-d <raashidm862@gmail.com>
2023-12-23 17:21:46 +0530

    small bug

--- hotnfast.sql
+++ hotnfast.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 22, 2023 at 10:43 AM
+-- Generation Time: Dec 23, 2023 at 11:47 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -39,6 +39,7 @@ CREATE TABLE IF NOT EXISTS `checkout` (
   `cardnumber` bigint NOT NULL,
   `expnumber` int NOT NULL,
   `cvvnumber` int NOT NULL,
+  `payment_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`order_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -46,8 +47,8 @@ CREATE TABLE IF NOT EXISTS `checkout` (
 -- Dumping data for table `checkout`
 --
 
-INSERT INTO `checkout` (`order_id`, `fullname`, `email`, `address`, `city`, `zipcode`, `cardname`, `cardnumber`, `expnumber`, `cvvnumber`) VALUES
-(1, 'Mohamed Raashid', 'mhmdrashid@gmail.com', '32/2, polgolla road, Udathalawinna', 'Kandy', 20802, 0, 1234123412341234, 125, 123);
+INSERT INTO `checkout` (`order_id`, `fullname`, `email`, `address`, `city`, `zipcode`, `cardname`, `cardnumber`, `expnumber`, `cvvnumber`, `payment_time`) VALUES
+(1, 'Mohamed Raashid', 'mhmdrashid@gmail.com', '32/2, polgolla road, Udathalawinna', 'Kandy', 20802, 0, 1234123412341234, 125, 123, '2023-12-23 11:43:02');
 
 -- --------------------------------------------------------
 
@@ -65,6 +66,7 @@ CREATE TABLE IF NOT EXISTS `register` (
   `pword` varchar(50) NOT NULL,
   `Confirmpw` varchar(50) NOT NULL,
   `gender` enum('male','female','notprefer') NOT NULL,
+  `registration_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -72,8 +74,8 @@ CREATE TABLE IF NOT EXISTS `register` (
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`id`, `fname`, `Uname`, `Email`, `Pnumber`, `pword`, `Confirmpw`, `gender`) VALUES
-(10, 'Mohammed Raashid', 'Raashid', 'raashid@gmail.com', 2147483647, '12345', '12345', 'male');
+INSERT INTO `register` (`id`, `fname`, `Uname`, `Email`, `Pnumber`, `pword`, `Confirmpw`, `gender`, `registration_time`) VALUES
+(10, 'Mohammed Raashid', 'Raashid', 'raashid@gmail.com', 2147483647, '12345', '12345', 'male', '2023-12-23 11:44:28');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
